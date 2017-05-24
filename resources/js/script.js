19b98bfd1cc95778e4e2b1e0ad31747c9aa36249
Eero Lehtonen <eero.lehtonen@aalto.fi>
2017-05-24 20:48:56 +0300

    Sticky nav with jQuery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
-
+ 
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+    /*
+    var waypoint = new Waypoint({
+        element: document.getElementById('waypoint'),
+        handler: function(direction) {
+            console.log('Scrolled to waypoint!')
+        }
+    })
+    */
 
 });
